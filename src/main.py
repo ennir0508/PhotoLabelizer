f32472e166c4e0e9ad03a21d24de726da2435818
ebikatsu <ennir0508+pg@gmail.com>
2023-11-29 00:30:34 +0900

    format and add comment

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
+"""This is a main program."""
+import glob
 import os
 import sys
-import glob
 
 from name_generator import NameGenerator as ng
-from multiprocessing import Pool, Process
-from threading import Thread
 
-# 日本語キャプションを生成する
+
 def generate_caption(filepath):
+    """
+    日本語キャプションを生成する
+    """
     # キャプションの自動生成(英語)
     caption = ng.generate(filepath)
     # キャプションの日本語化
-    caption_ja = ng.translate(caption) 
+    caption_ja = ng.translate(caption)
 
     return {
         "filepath": filepath,
@@ -19,18 +21,22 @@ def generate_caption(filepath):
         "caption_ja": caption_ja,
     }
 
-# ファイル名を変更する
-# @param filepath 対象ファイル
-# @param caption 説明
-# @param caption_ja 変更後ファイル名(拡張子無)
+
 def rename_file(filepath, caption, caption_ja):
+    """
+    ファイル名を変更する
+    @param filepath 対象ファイル
+    @param caption 説明
+    @param caption_ja 変更後ファイル名(拡張子無)
+    """
+
     # ファイル名
     basename = os.path.basename(filepath)
     # 拡張子無ファイル名
     basename_without_ext = os.path.splitext(basename)[0]
-    # .拡張子 
+    # .拡張子
     ext = os.path.splitext(os.path.basename(filepath))[1]
-    
+
     info = {
         "変更前    :": basename,
         "説明(英語):": caption,
@@ -39,18 +45,18 @@ def rename_file(filepath, caption, caption_ja):
     print("")
     for key, value in info.items():
         print(key, value)
-    
+
     # ユーザーの確認を促す
-    answer = input('ファイル名を変更しますか？(Y/N): ')
-    if answer[0].lower() == 'y':
+    answer = input("ファイル名を変更しますか？(Y/N): ")
+    if answer[0].lower() == "y":
         new_filepath = filepath.replace(basename_without_ext, caption_ja)
         os.rename(os.path.abspath(filepath), os.path.abspath(new_filepath))
         print("ファイル名を変更しました")
     else:
         print("ファイル名を変更しませんでした")
 
-# main 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = sys.argv
     if 2 <= len(args):
         # 実行ファイル名は除く
@@ -58,15 +64,26 @@ if __name__ == '__main__':
         # 引数に対してそれぞれ
         for arg in args:
             # ファイルが存在する場合
-            if os.path.isfile(arg): 
+            if os.path.isfile(arg):
                 caption_data = generate_caption(arg)
-                rename_file(caption_data["filepath"], caption_data["caption"], caption_data["caption_ja"])
+                rename_file(
+                    caption_data["filepath"],
+                    caption_data["caption"],
+                    caption_data["caption_ja"],
+                )
             # ディレクトリが存在する場合
             elif os.path.exists(arg):
                 filelist = glob.glob(arg + "*.jpg")
-                caption_data_list = list(map(lambda file: generate_caption(file), filelist))
-                list(map(lambda cap: rename_file(cap["filepath"], cap["caption"], cap["caption_ja"]), caption_data_list))
+                caption_data_list = list(map(generate_caption, filelist))
+                list(
+                    map(
+                        lambda cap: rename_file(
+                            cap["filepath"], cap["caption"], cap["caption_ja"]
+                        ),
+                        caption_data_list,
+                    )
+                )
             else:
                 print("存在しないファイルまたはディレクトリです: " + arg)
     else:
-        print('引数がありません')
+        print("引数がありません")
--- src/name_generator.py
+++ src/name_generator.py
@@ -1,24 +1,31 @@
-import os
+"""This is a main program."""
+
 import torch
-
-from PIL import Image
-from lavis.models import load_model_and_preprocess
 from googletrans import Translator
+from lavis.models import load_model_and_preprocess
+from PIL import Image
 
-# 引数による画像ファイル取得
+
 class NameGenerator:
-    
-    # 画像からキャプションの自動生成
-    def generate(filepath):
+    """
+    名前生成クラス
+    """
+
+    def generate(self, filepath):
+        """
+        画像からキャプションの自動生成
+        """
         # setup device to use
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # load sample image
         raw_image = Image.open(filepath).convert("RGB")
-    
+
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # loads BLIP caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
         # this also loads the associated image processors
-        model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
+        model, vis_processors, _ = load_model_and_preprocess(
+            name="blip_caption", model_type="base_coco", is_eval=True, device=device
+        )
         # preprocess the image
         # vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
         image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
@@ -26,9 +33,11 @@ class NameGenerator:
         caption_data = model.generate({"image": image})
         caption = caption_data[0]
         return caption
-        
-    # キャプションの日本語化
-    def translate(text_en):
+
+    def translate(self, text_en):
+        """
+        キャプションの日本語化
+        """
         translator = Translator()
-        text_ja = translator.translate(text_en, src='en', dest='ja').text
+        text_ja = translator.translate(text_en, src="en", dest="ja").text
         return text_ja
